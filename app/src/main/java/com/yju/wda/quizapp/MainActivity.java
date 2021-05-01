3b2b18d506ed6e21eaa109db815a6c9b719eef83
limkk90 <44752388+limkk90@users.noreply.github.com>
2021-05-01 23:07:06 +0900

    MainActivity Javacode process 1/5

--- app/src/main/java/com/yju/wda/quizapp/MainActivity.java
+++ app/src/main/java/com/yju/wda/quizapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.yju.wda.quizapp.databinding.ActivityMainBinding;
 
@@ -14,27 +15,36 @@ public class MainActivity extends AppCompatActivity {
     String adminPwd = "12345"; //설정 비밀번호
     ActivityMainBinding mainBinding;
     ImageView imgAdminBtn;
-    ConstraintLayout layout;
+    ConstraintLayout mainLayout, passwordLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(mainBinding.getRoot());
 //        imgAdminBtn = (ImageView)findViewById(R.id.imgAdminButton);
-
-        mainBinding.imgAdminButton.setOnClickListener(new View.OnClickListener() {
+        mainLayout = findViewById(R.id.mainLayout);
+        passwordLayout = findViewById(R.id.passwordLayout);
+        mainBinding.mainLayout.setVisibility(View.VISIBLE);
+        mainBinding.passwordLayout.setVisibility(View.GONE);
+        mainBinding.imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i("Main:" , "이미지버튼 눌림");
                 mainBinding.mainLayout.setVisibility(View.GONE);
                 mainBinding.passwordLayout.setVisibility(View.VISIBLE);
-                String insertPwd = mainBinding.pwdEditText.getText().toString();
-                if(adminPwd.equals(insertPwd)){
-                    //인텐트 이동 ListAciticty로
-                }else{
-                    mainBinding.mainLayout.setVisibility(View.VISIBLE);
-                    mainBinding.passwordLayout.setVisibility(View.GONE);
-                }
+
+                mainBinding.pwdBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String insertPwd = mainBinding.pwdEditText.getText().toString();
+                        if (adminPwd.equals(insertPwd)){
+                            //인텐트 이동
+                        }else{
+                            mainBinding.mainLayout.setVisibility(View.VISIBLE);
+                            mainBinding.passwordLayout.setVisibility(View.GONE);
+                            Toast.makeText(MainActivity.this, "비번틀림", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
     }
