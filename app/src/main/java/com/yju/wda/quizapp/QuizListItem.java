a6f16ecf02c8ebdbbbca7245214af011267d4381
limkk90 <44752388+limkk90@users.noreply.github.com>
2021-05-05 22:50:29 +0900

    add ListItem

--- app/src/main/java/com/yju/wda/quizapp/QuizListItem.java
+++ app/src/main/java/com/yju/wda/quizapp/QuizListItem.java
@@ -1,6 +1,43 @@
 package com.yju.wda.quizapp;
 
+import android.graphics.drawable.Drawable;
+
 public class QuizListItem {
-    String title;
-    String
+    Drawable iconDrawable;
+    String pTitle;
+    String pRegDate;
+
+
+    @Override
+    public String toString() {
+        return "QuizListItem{" +
+                "iconDrawable=" + iconDrawable +
+                ", pTitle='" + pTitle + '\'' +
+                ", pRegDate='" + pRegDate + '\'' +
+                '}';
+    }
+
+    public Drawable getIconDrawable() {
+        return iconDrawable;
+    }
+
+    public void setIconDrawable(Drawable iconDrawable) {
+        this.iconDrawable = iconDrawable;
+    }
+
+    public String getpTitle() {
+        return pTitle;
+    }
+
+    public void setpTitle(String pTitle) {
+        this.pTitle = pTitle;
+    }
+
+    public String getpRegDate() {
+        return pRegDate;
+    }
+
+    public void setpRegDate(String pRegDate) {
+        this.pRegDate = pRegDate;
+    }
 }
